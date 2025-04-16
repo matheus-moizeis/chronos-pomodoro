3880af2d4b0fd00cc2aff07ef67a33b137b511cc
Matheus Moizeis <matheusmoizeis@outlook.com>
2025-04-16 18:05:47 -0300

    Ajustes iniciais

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       </Container>
       <Container>
         <Menu />
+      </Container>
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
