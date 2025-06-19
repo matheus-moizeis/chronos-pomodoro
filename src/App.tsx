9f59a548a6b51e486ffec2d1322c9af4cfb72e47
Matheus Moizeis <matheus.moizeis@outlook.com>
2025-06-18 23:52:29 -0300

    organizando projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,7 @@
-import './styles/theme.css';
-import './styles/global.css';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/DefaultInput';
-import Cycles from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircle } from 'lucide-react';
-import { Footer } from './components/Footer';
+import { Home } from './pages/Home';
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-      <Container>
-        <Menu />
-      </Container>
-      <Container>
-        <CountDown />
-      </Container>
-      <Container>
-        <form className='form' action=''>
-          <div className='formRow'>
-            <DefaultInput
-              id='num'
-              type='text'
-              labelText='Task'
-              placeholder='Digite algo'
-            />
-          </div>
-          <div className='formRow'>
-            <p>Lorem ipsum dolor sit amet.</p>
-          </div>
-          <div className='formRow'>
-            <Cycles />
-          </div>
-          <div className='formRow'>
-            <DefaultButton icon={<PlayCircle />} color='green' />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
